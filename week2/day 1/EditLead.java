5bbc7c9a146ec9520c7b5daba8b9ff116cb647bb
VaanmathiKrish <140222493+VaanmathiKrish@users.noreply.github.com>
2023-07-23 08:16:24 +0530

    Create Lead & Update Lead Selenium code uploading

---
+++ week2/day 1/EditLead.java
@@ -0,0 +1,88 @@
+package week2.day1.assignment;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class EditLead {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Load driver
+		ChromeDriver driver = new ChromeDriver();
+
+		//Load URL
+		driver.get("http://leaftaps.com/opentaps/control/login");
+
+		//Maximize window
+		driver.manage().window().maximize();
+
+		// ASSIGNMENT 1
+		// CREATE LEAD
+		
+		//Locate username and password and populate
+		driver.findElement(By.id("username")).sendKeys("Demosalesmanager"); 
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+
+		//Locate Login button & click
+		driver.findElement(By.className("decorativeSubmit")).click();
+
+		//Locate CRM/SFA link & click
+		driver.findElement(By.linkText("CRM/SFA")).click();
+
+		//Locate Leads tab link & click
+		driver.findElement(By.linkText("Leads")).click();
+
+		//Locate Create Lead link & click
+		driver.findElement(By.linkText("Create Lead")).click();
+
+		//Locate CompanyName and populate
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("KPMG pvt Ltd"); 
+
+		//Locate FirstName and populate
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Jennifer"); 
+
+		//Locate LastName and populate
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Carol"); 
+
+		//Locate FirstName(Local) and populate
+		driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("Jenni"); 
+
+		//Locate Department and populate
+		driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("Sales"); 
+
+		//Locate Description and populate
+		driver.findElement(By.id("createLeadForm_description")).sendKeys("Worked in Sales "); 
+
+		//Locate Email address and populate
+		driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("jcarol@gmail.com"); 
+
+		//Locate State/Province dropdown 
+		WebElement element = driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+		Select option = new Select(element);
+		//populate with one of the dropdown values
+		option.selectByVisibleText("New York");
+
+		//Locate Create Lead button & click
+		driver.findElement(By.className("smallSubmit")).click();
+
+		//ASSIGNMENT 2
+		//EDIT LEAD
+
+		//Locate Edit button & click
+		driver.findElement(By.linkText("Edit")).click();
+
+		//Locate Description and clear it
+		driver.findElement(By.id("updateLeadForm_description")).clear();
+
+		//Locate ImportantNote and clear it
+		driver.findElement(By.id("updateLeadForm_importantNote")).sendKeys("Lead details updated"); 
+
+		//Locate Update button & click
+		//Using xpath since NO unique id or class name is available
+		driver.findElement(By.xpath("//input[@type='submit' and @value='Update']")).click();
+	}
+
+}
